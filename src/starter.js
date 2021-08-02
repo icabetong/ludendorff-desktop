80adfc98c1a74158ec35868be8c4578a5a03e3f6
Isaiah Collins Abetong <isaiahcollins_02@live.com>
2021-08-02 21:19:33 +0800

    Added electron

--- src/starter.js
+++ src/starter.js
@@ -1,35 +1,30 @@
 const electron = require('electron');
 const app = electron.app;
-
 const BrowserWindow = electron.BrowserWindow;
 
 const path = require('path');
-const url = require('url');
+const isDev = require('electron-is-dev');
 
 let mainWindow;
 
 function createWindow() {
-    mainWindow = new BrowserWindow({width: 800, height: 600})
-    mainWindow.loadURL('http://localhost:3000')
+    mainWindow = new BrowserWindow({width: 800, height: 600});
 
-    mainWindow.on('closed', function() {
-        mainWindow = null
-    })
+    const appUrl = isDev ? 'http://localhost:3000' 
+        : `file://${path.join(__dirname, '../build/index.html')}`;
+    mainWindow.loadURL(appUrl);
+
+    mainWindow.on('closed', () => { mainWindow = null })
 }
 
 app.on('ready', createWindow);
-app.on('window-all-closed', function() {
+app.on('window-all-closed', () => {
     // On OS X it is common for applications and their menu bar
     // to stay active until the user quits explicitly with Cmd + Q
-    if (process.plaform !== 'darwin') {
-        app.quit()
-    }
+    if (process.plaform !== 'darwin') { app.quit() }
 })
-
-app.on('activate', function () {
+app.on('activate', () => {
     // On OS X it's common to re-create a window in the app when the
     // dock icon is clicked and there are no other windows open.
-    if (mainWindow === null) {
-        createWindow()
-    }
+    if (mainWindow === null) { createWindow() }
 });
